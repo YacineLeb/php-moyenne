127473fb70af06fdbb77d7a4264dafca5f536c9e
LEBKIRI YACINE <lebkiri_y@JEAN-ROSTAND.lan>
2023-09-25 09:49:40 +0200

    moyenne ok

--- exo2/main.php
+++ exo2/main.php
@@ -1,9 +1,9 @@
 <?php
 include ("statistique.php");
-$tab = array(1500,4500,2200,1500,3300,1800,1700,4000);
+$tab = array(1500,4500,2200,1500,3300,1800,1700);
 
-//echo "La moyenne est de : ". moyenne($tab);
-//echo "\n";
+echo "La moyenne est de : ". moyenne($tab);
+echo "\n";
 echo "La médiane est de : ". medianee($tab);
 
 //var_dump(trie($tab));
